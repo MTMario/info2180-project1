51361fbd4a9e343606aac6b026e8b6ee1fedcf11
Mario Thompson <mariothompson181998@gmail.com>
2022-10-27 18:37:09 -0500

    Edited js file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
 /* Add your JavaScript to this file */
+window.onload = function() {
+document.getElementsByClassName(".btn").onclick = function() {
+
+    let email = document.querySelector("#email");
+    let message = document.querySelector(".message");
+
+    message.innerHTML = "Thank you! Your email address" + email + "has been added to our mailing list!";
+
+} 
+}
